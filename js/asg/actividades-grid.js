d7b07d4e3f1e2c28cff75e9dab7e4292abefb9d5
edcan-dev <israel.da.queenie@gmail.com>
2023-11-27 19:28:11 -0600

    20231127 - ADDED - ASG SOCIAL & AMBIENTAL

--- js/asg/actividades-grid.js
+++ js/asg/actividades-grid.js
@@ -1,103 +1,138 @@
-// const jsonResponse = await fetch('../../data/asg/actividades-data.json');
-const jsonResponse = await fetch('https://edcan-dev.github.io/planigrupo-edcan-dev/data/asg/actividades-data.json');
-const actividades = await jsonResponse.json();
+const renderGridItemFromRow = (row) => {
+  const item = document.createElement("div");
+  item.classList.add("actividades__grid__item");
 
-const createGridItem = (json) => {
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("actividades__grid__item__title");
+  const titleContainerP = document.createElement("p");
+  titleContainerP.innerHTML = row[1];
+  titleContainer.appendChild(titleContainerP);
 
-    const item = document.createElement('div');
-    item.classList.add('actividades__grid__item');
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("actividades__grid__item__img");
+  imgContainer.style.backgroundImage = "url('" + row[8] + "')";
 
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('actividades__grid__item__title');
-    const titleContainerP = document.createElement('p');
-    titleContainerP.innerHTML = json.name;
-    titleContainer.appendChild(titleContainerP);
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.classList.add("actividades__grid__item__description");
+  const descriptionContainerP = document.createElement("p");
+  descriptionContainerP.innerHTML = row[9];
+  descriptionContainer.appendChild(descriptionContainerP);
 
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('actividades__grid__item__img');
-    imgContainer.style.backgroundImage = "url('" + json.image_url + "')";
+  const anchorContainer = document.createElement("div");
+  anchorContainer.classList.add("actividades__grid__item__a");
+  const anchorContainerA = document.createElement("a");
+  anchorContainerA.innerHTML = "VER MÁS";
+  anchorContainer.appendChild(anchorContainerA);
 
-    const descriptionContainer = document.createElement('div');
-    descriptionContainer.classList.add('actividades__grid__item__description');
-    const descriptionContainerP = document.createElement('p');
-    descriptionContainerP.innerHTML = json.description;
-    descriptionContainer.appendChild(descriptionContainerP);
+  item.appendChild(titleContainer);
+  item.appendChild(imgContainer);
+  item.appendChild(descriptionContainer);
+  item.appendChild(anchorContainer);
 
-    const anchorContainer = document.createElement('div');
-    anchorContainer.classList.add('actividades__grid__item__a');
-    const anchorContainerA = document.createElement('a');
-    anchorContainerA.innerHTML = 'VER MÁS';
-    anchorContainer.appendChild(anchorContainerA);
+  document.querySelector(".actividades__grid").appendChild(item);
+};
 
+const renderGridItemsFromRows = (rows) => {
+  rows.forEach((row) => {
+    const item = document.createElement("div");
+    item.classList.add("actividades__grid__item");
 
-    item.appendChild(titleContainer)
-    item.appendChild(imgContainer)
-    item.appendChild(descriptionContainer)
-    item.appendChild(anchorContainer)
-
-    document.querySelector('.actividades__grid').appendChild(item);
-}
-const createGridItemFromRow = (row) => {
-
-    console.log(row);
-
-    const json = ''
-
-    const item = document.createElement('div');
-    item.classList.add('actividades__grid__item');
-
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('actividades__grid__item__title');
-    const titleContainerP = document.createElement('p');
+    const titleContainer = document.createElement("div");
+    titleContainer.classList.add("actividades__grid__item__title");
+    const titleContainerP = document.createElement("p");
     titleContainerP.innerHTML = row[1];
     titleContainer.appendChild(titleContainerP);
 
-    const imgContainer = document.createElement('div');
-    imgContainer.classList.add('actividades__grid__item__img');
+    const imgContainer = document.createElement("div");
+    imgContainer.classList.add("actividades__grid__item__img");
     imgContainer.style.backgroundImage = "url('" + row[8] + "')";
 
-    const descriptionContainer = document.createElement('div');
-    descriptionContainer.classList.add('actividades__grid__item__description');
-    const descriptionContainerP = document.createElement('p');
+    const descriptionContainer = document.createElement("div");
+    descriptionContainer.classList.add("actividades__grid__item__description");
+    const descriptionContainerP = document.createElement("p");
     descriptionContainerP.innerHTML = row[9];
     descriptionContainer.appendChild(descriptionContainerP);
 
-    const anchorContainer = document.createElement('div');
-    anchorContainer.classList.add('actividades__grid__item__a');
-    const anchorContainerA = document.createElement('a');
-    anchorContainerA.innerHTML = 'VER MÁS';
+    const anchorContainer = document.createElement("div");
+    anchorContainer.classList.add("actividades__grid__item__a");
+    const anchorContainerA = document.createElement("a");
+    anchorContainerA.innerHTML = "VER MÁS";
     anchorContainer.appendChild(anchorContainerA);
 
+    item.appendChild(titleContainer);
+    item.appendChild(imgContainer);
+    item.appendChild(descriptionContainer);
+    item.appendChild(anchorContainer);
+    document.querySelector(".actividades__grid").appendChild(item);
+  });
+};
 
-    item.appendChild(titleContainer)
-    item.appendChild(imgContainer)
-    item.appendChild(descriptionContainer)
-    item.appendChild(anchorContainer)
+const clearGrid = () => {
+  document.querySelector(".actividades__grid").innerHTML = "";
+};
 
-    document.querySelector('.actividades__grid').appendChild(item);
-}
-
-//const xslxPath = './../../data/asg/asg_actividades.xlsx';
-
-const xslxPath = 'https://edcan-dev.github.io/planigrupo-edcan-dev/data/asg/asg_actividades.xlsx';
+const xslxPath =
+  "https://edcan-dev.github.io/planigrupo-edcan-dev/data/asg/asg_actividades.xlsx";
 const response = await fetch(xslxPath);
 const xlsx = await response.blob();
 
-readXlsxFile(xlsx, { sheet: 1 }).then(function(rows) {
-    rows.shift();
-    rows.forEach(row => createGridItemFromRow(row))
-})
+let actividades = [];
+let filteredActividades = [];
 
+await readXlsxFile(xlsx, { sheet: 1 }).then(function (rows) {
+  actividades = rows;
+});
 
-//actividades.forEach( json => createGridItem(json));
+actividades.shift();
+renderGridItemsFromRows(actividades);
+//actividades.forEach((actividad) => renderGridItemFromRow(actividad));
 
+document.querySelector("#category_select").addEventListener("change", (ev) => {
+  const categoryValue = ev.target.value;
+  const yearValue = document.querySelector("#year_select").value;
 
-const je =
-{
-    "id": 1,
-    "name": "Máquinas de Reciclaje-AIEn",
-    "image_url": "https://planiesg.herokuapp.com/static/images/eventos_soc/Reciclar-para-ganar-abril-2022-1.png",
-    "description": "De enero-abril 2022 se reciclaron más de 300 mil botellas de plástico equivalentes a una reducción de 17,642 kg de CO2",
-    "date": "2022-06-17",
-    "category": "Ambientales"  
+  if (yearValue == "all") {
+    if (categoryValue === "all") {
+      filteredActividades = actividades;
+    } else {
+      filteredActividades = actividades.filter(
+        (actividad) => actividad[0] === categoryValue
+      );
+    }
+  } else {
+    if (categoryValue === "all") {
+      filteredActividades = actividades.filter((actividad) => {
+        return actividad[5] == yearValue;
+      });
+    } else {
+      filteredActividades = actividades.filter((actividad) => {
+        return actividad[0] === categoryValue && actividad[5] == yearValue;
+      });
+    }
   }
+
+  clearGrid();
+  renderGridItemsFromRows(filteredActividades);
+});
+
+document.querySelector("#year_select").addEventListener("change", (ev) => {
+  const yearValue = ev.target.value;
+  const categoryValue = document.querySelector("#category_select").value;
+
+  if(categoryValue == 'all') {
+    filteredActividades = actividades.filter(
+        actividad => actividad[5] == yearValue
+    );
+
+  } else {
+
+    filteredActividades = actividades.filter(
+        actividad => 
+            actividad[5] == yearValue &&
+            actividad[0] == categoryValue
+    );
+  }
+
+  clearGrid();
+  renderGridItemsFromRows(filteredActividades);
+});
