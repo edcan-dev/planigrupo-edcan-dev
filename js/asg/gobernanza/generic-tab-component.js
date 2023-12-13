05b6b32637222ce171e17b03910325c5f44187a5
edcan-dev <israel.da.queenie@gmail.com>
2023-12-13 13:12:56 -0600

    202312123 - Forms Tests

---
+++ js/asg/gobernanza/generic-tab-component.js
@@ -0,0 +1,40 @@
+class GenericTabsComponent {
+  constructor(tabsSelector, tabsContentSelector) {
+    this.tabsSelector = tabsSelector;
+    this.tabsContentSelector = tabsContentSelector;
+  }
+
+  initialize() {
+    document.querySelectorAll(this.tabsSelector).forEach((selector, i, list) =>
+      selector.addEventListener("click", (ev) => {
+        list.forEach((s) => {
+          s.classList.remove("selected");
+          if (s.id === ev.target.id) s.classList.add("selected");
+        });
+
+        const tabContents = document.querySelectorAll(this.tabsContentSelector);
+
+        const idCharArray = ev.target.id.split("");
+        const idNumber = idCharArray.pop();
+
+        tabContents.forEach((contents) => {
+          contents.classList.add("inactive");
+          if (contents.id.includes(idNumber))
+            contents.classList.remove("inactive");
+        });
+      })
+    );
+  }
+}
+
+
+const goberTabs = new GenericTabsComponent('.gober_asg_selector','.gober_asg_content').initialize();
+
+const goberSubtabs = new GenericTabsComponent(
+  '.gober_asg_content--second__tabs__selector__item',
+  '.gober_asg_content--second__tabs__content'
+).initialize();
+
+const susSection2Subtabs = new GenericTabsComponent(
+  '.susAsg_contSecond_topics__tabs__selectors',
+  '.susAsg_contSecond_topics__tabs__content').initialize();
