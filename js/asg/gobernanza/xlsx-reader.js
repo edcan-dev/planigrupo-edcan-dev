f1203041ba10fdb2d1568df549f3fc6b9bca1d42
edcan-dev <israel.da.queenie@gmail.com>
2024-01-11 12:03:50 -0600

    FIXED - FILES05

--- js/asg/gobernanza/xlsx-reader.js
+++ js/asg/gobernanza/xlsx-reader.js
@@ -1,38 +1,43 @@
-const xslxPath = "https://edcan-dev.github.io/planigrupo-edcan-dev/data/asg/asg_actividades.xlsx";
+// const gobernanzaActivities = [];
+// try {
+//   const xslxPath = "https://edcan-dev.github.io/planigrupo-edcan-dev/data/asg/asg_actividades.xlsx";
 
-const response = await fetch(xslxPath);
-const xlsx = await response.blob();
+// const response = await fetch(xslxPath);
+// const xlsx = await response.blob();
 
-class ActivityDetail {
-  id;  
-  title;
-  imgUrl;
-  description;
-  category;
-  dateString;
-}
+// class ActivityDetail {
+//   id;  
+//   title;
+//   imgUrl;
+//   description;
+//   category;
+//   dateString;
+// }
 
-const gobernanzaActivities = [];
-const searchedCategory = 'Gobernanza';
 
-await readXlsxFile(xlsx, { sheet: 1 }).then(function (rows) {
+// await readXlsxFile(xlsx, { sheet: 1 }).then(function (rows) {
+  
+  //   for (let i = 0; i < rows.length; i++) {
+    //     const element = rows[i];
+    // //    console.log(element);
+    //     if(element[0] == searchedCategory) {
+      //       gobernanzaActivities.push(element)
+      //     }
+      //   }
+      // });
+      // } catch(e) {
+        
+        // }
+        
+        
+        
+if (true) {
+  const searchedCategory = "Gobernanza";
+  console.warn(`[NOT FOUND ACTIVITIES FOR ${searchedCategory} CATEGORY]`);
 
-  for (let i = 0; i < rows.length; i++) {
-    const element = rows[i];
-//    console.log(element);
-    if(element[0] == searchedCategory) {
-      gobernanzaActivities.push(element)
-    }
-  }
-});
-
-if(gobernanzaActivities.length == 0) {
-  console.warn(`[NOT FOUND ACTIVITIES FOR ${ searchedCategory } CATEGORY]`);
- 
-  document.querySelector('.actividades').innerHTML =
-  `
-    <div class="actividades__notFound">
-      <span>No hay actividades para la categoría ${ searchedCategory }</span>
-    </div>
-  `
+  document.querySelector(".actividades").innerHTML = `
+  <div class="actividades__notFound">
+  <span>No hay actividades para la categoría ${searchedCategory}</span>
+  </div>
+  `;
 }
